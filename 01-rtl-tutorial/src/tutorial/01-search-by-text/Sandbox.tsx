40a3c6d4d3d8c6e75bf27986c2c9a6b1932bd6d1
victor <oguntayovictor65@gmail.com>
2025-09-13 01:06:15 +0100

    work on search by text sandbox

--- 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
+++ 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+  const [showMessage, setShowMessage] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div>
+      <h2>React Testing Library Example</h2>
+      <p>You can search me with regular expression</p>
+      {showError && <p>Error Message</p>}
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+      </ul>
+      {showMessage && <p>Async Message</p>}
+    </div>
+  );
 };
 export default Sandbox;
