581f6653aff6543d3c6d4a8d825842e6ba79ba57
victor <oguntayovictor65@gmail.com>
2025-12-29 08:02:01 +0100

    test for form element initially empty on focus flow app

---
+++ 03-tdd-focus-flow-project/01-focus-flow-starter/src/__tests__/Form.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import userEvent, { UserEvent } from "@testing-library/user-event";
+import { describe } from "vitest";
+import Form from "../components/Form";
+
+export const getFormElements = () => {
+  return {
+    titleInput: screen.getByRole("textbox", { name: /title/i }),
+    descriptionInput: screen.getByRole("textbox", { name: /description/i }),
+    categorySelect: screen.getByRole("combobox", { name: /category/i }),
+    submitButton: screen.getByRole("button", { name: /add task/i }),
+  };
+};
+
+describe("form component", () => {
+  let user: UserEvent;
+  const mockOnSubmit = vi.fn();
+
+  beforeEach(() => {
+    user = userEvent.setup();
+    mockOnSubmit.mockClear();
+    render(<Form onSubmit={mockOnSubmit} />);
+  });
+
+  test("render empty form inputs initially", () => {
+    const { titleInput, descriptionInput, categorySelect } = getFormElements();
+
+    expect(titleInput).toHaveValue("");
+    expect(descriptionInput).toHaveValue("");
+    expect(categorySelect).toHaveValue("");
+  });
+});
--- 03-tdd-focus-flow-project/01-focus-flow-starter/src/components/Form.tsx
+++ 03-tdd-focus-flow-project/01-focus-flow-starter/src/components/Form.tsx
@@ -1,6 +1,80 @@
-import { ItemWithoutId } from "../utils";
+import { FormEvent, useState } from "react";
+import { ItemCategory, ItemWithoutId } from "../utils";
 
 const Form = ({ onSubmit }: { onSubmit: (item: ItemWithoutId) => void }) => {
-  return <form>Form</form>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState<ItemCategory | "">("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const labelStyles = "block leading-none font-medium mb-2 text-sm";
+  const inputStyles = "flex h-10 w-full rounded-md border px-3 py-2 text-sm";
+  return (
+    <div className="max-w-xl">
+      <h2 className="font-semibold text-xl mb-2">Add New Task</h2>
+      <form className="space-y-4">
+        <div className="">
+          <label htmlFor="title" className={labelStyles}>
+            Title
+          </label>
+          <input
+            type="text"
+            className={inputStyles}
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="description" className={labelStyles}>
+            Description
+          </label>
+          <input
+            type="text"
+            className={inputStyles}
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="category" className={labelStyles}>
+            Category
+          </label>
+          <select
+            name="category"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value as ItemCategory)}
+            className={inputStyles}
+          >
+            <option value=""> select Category</option>
+            <option value="urgent" className="capitalize">
+              {" "}
+              select Category
+            </option>
+            <option value="important" className="capitalize">
+              {" "}
+              important
+            </option>
+            <option value="normal" className="capitalize">
+              {" "}
+              normal
+            </option>
+            <option value="low">low</option>
+          </select>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 font-medium text-white text-sm h-10 px-4 py-2"
+        >
+          add task
+        </button>
+      </form>
+    </div>
+  );
 };
 export default Form;
